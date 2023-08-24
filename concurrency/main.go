7ed8b841439c44d23f8e8e6663d43074161c3386
Marcelo T. dos Santos <marcelo.teixeira@momox.biz>
2023-08-24 16:42:51 +0200

    Call function from main

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	concurrency "github.com/Marcelixoo/learn-go-with-tests/concurrency/src"
+)
+
+const (
+	GOOGLE_DOT_COM_URL  = "https://google.com"
+	SPOTIFY_DOT_COM_URL = "https://open.spotify.com/"
+	FAKE_WEBSITE_URL    = "waat://furhurterwe.geds"
+)
 
 func main() {
 	fmt.Println("running concurrency chapter...")
+
+	websites := []string{
+		GOOGLE_DOT_COM_URL,
+		SPOTIFY_DOT_COM_URL,
+		FAKE_WEBSITE_URL,
+	}
+
+	wc := func(url string) bool {
+		return url != FAKE_WEBSITE_URL
+	}
+
+	results := concurrency.CheckWebsites(wc, websites)
+
+	fmt.Printf("Results: %v", results)
 }
