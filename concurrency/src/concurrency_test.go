183a8e559c3a5d65cd5fa0ff152123986af23409
Marcelo T. dos Santos <marcelo.teixeira@momox.biz>
2023-08-29 00:52:03 +0200

    Add example of concurrent processes with context

--- concurrency/src/concurrency_test.go
+++ concurrency/src/concurrency_test.go
@@ -1,6 +1,7 @@
 package concurrency_test
 
 import (
+	"context"
 	"reflect"
 	"testing"
 	"time"
@@ -56,5 +57,123 @@ func BenchmarkCheckWebsites(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		concurrency.CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
+
+func SendMessageAfter(_ time.Duration, out chan string, msg string, ctx context.Context) {
+	// time.Sleep(delayInSeconds * time.Second)
+	select {
+	case <-ctx.Done():
+		return
+	default:
+		out <- msg
+	}
+}
+
+type SubRoutine interface {
+	Run()
+}
+
+func RunMainProcess(subroutines []func(), out chan string, retries int) []string {
+	for _, run := range subroutines {
+		go run()
+	}
+
+	var results []string
+
+	for i := 0; i < retries; i++ {
+		select {
+		case msg := <-out:
+			results = append(results, msg)
+		default:
+			results = append(results, "neither routine was ready")
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	return results
+}
+
+func RunMainProcessWithoutRetries(subroutines []func(), out chan string, cancel context.CancelFunc) []string {
+	for _, run := range subroutines {
+		go run()
+	}
+
+	defer cancel()
+
+	var results []string
+	for msg := range out {
+		results = append(results, msg)
+	}
+	return results
+}
+
+func TestSelectStatementForInitialization(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	out := make(chan string)
+
+	t.Run("it waits till some of the channels is ready to communicate", func(t *testing.T) {
+		r1 := func() {
+			SendMessageAfter(1, out, "received message from subroutine 1", ctx)
+		}
+		r2 := func() {
+			SendMessageAfter(3, out, "received message from subroutine 2", ctx)
+		}
+		numOfRetries := 5
+
+		got := RunMainProcess([]func(){r1, r2}, out, numOfRetries)
+
+		assertNumberOfMessages(t, got, numOfRetries)
+
+		assertFirstMessage(t, got, "neither routine was ready")
+		assertLastMessage(t, got, "received message from subroutine 2")
+	})
+
+	t.Run("it drains the channel til the end", func(t *testing.T) {
+		after1Sec := func() {
+			SendMessageAfter(1, out, "received message from subroutine 1", ctx)
+		}
+
+		got := RunMainProcessWithoutRetries([]func(){
+			after1Sec,
+			after1Sec,
+			after1Sec,
+		}, out, cancel)
+
+		defer cancel()
+
+		assertNumberOfMessages(t, got, 2)
+	})
+}
+
+func assertNumberOfMessages(t *testing.T, got []string, want int) {
+	t.Helper()
+	numOfMessages := len(got)
+	if numOfMessages != want {
+		t.Errorf("expected %d messages got %d", want, numOfMessages)
+	}
+}
+
+func assertFirstMessage(t *testing.T, got []string, match string) {
+	t.Helper()
+
+	position := 0
+	first := got[position]
+
+	if first != match {
+		t.Errorf("unexpected results; want %q got %q at position %d", match, first, position)
+	}
+}
+
+func assertLastMessage(t *testing.T, got []string, match string) {
+	t.Helper()
+
+	position := len(got) - 1
+	last := got[position]
+
+	if last != match {
+		t.Errorf("unexpected results; want %q got %q at position %d", match, last, position)
 	}
 }
